4f625c4ebdfe60fdb1dabe804d29fd7b8b5e0be1
Rayhan Abdullah <programmer.rayhan1@gmail.com>
2025-03-17 12:32:19 +0600

    fix majority error

--- config/db.ts
+++ config/db.ts
@@ -3,7 +3,9 @@ import { config } from "./env";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(config.url as string);
+    await mongoose.connect(config.url as string, {
+      writeConcern: { w: 'majority' },
+    });
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error);
