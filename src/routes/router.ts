1592c9c289b88d6ad692ea79c15407e2567ce2fb
Rayhan Abdullah <programmer.rayhan1@gmail.com>
2025-03-17 10:34:56 +0600

    remove-health-endpoint

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,11 +3,6 @@ import auth from '../api/auth';
 import notes from '../api/notes';
 
 const router = express.Router();
-router.get('/health',
-    (_req: Request, res: Response) => {
-    res.status(200).json({ status: 'ok', code: 200, message: 'Welcome to the API' });
-});
-
 router.use('/auth', auth.routes);
 router.use('/notes', notes.routes);
 
